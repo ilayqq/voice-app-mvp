b58dbb1dcc37aa270001e56c3c6455768eb4b004
Ахметжанов Самир <samira@halykbank.kz>
2026-06-02 16:13:28 +0500

    change config api urls

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -1,7 +1,7 @@
-// const DEFAULT_API_BASE_URL = 'https://sam.x64.kz/api/v1'
-// const AUTH_API_BASE_URL = 'https://sam.x64.kz/api'
-const AUTH_API_BASE_URL = 'http://localhost:8080'
-const DEFAULT_API_BASE_URL = 'http://localhost:8080/v1'
+const DEFAULT_API_BASE_URL = 'https://sam.x64.kz/api/v1'
+const AUTH_API_BASE_URL = 'https://sam.x64.kz/api'
+// const AUTH_API_BASE_URL = 'http://localhost:8080'
+// const DEFAULT_API_BASE_URL = 'http://localhost:8080/v1'
 
 export const API_CONFIG = {
   get baseURL(): string {
